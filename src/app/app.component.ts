290f7d81084e2c0bb6ac1fbba8f798a45923a658
Rosa M. Burrueco <rosam.burrueco@juntadeandalucia.es>
2022-03-04 12:13:08 +0100

    Métodos de componentes y uso en la plantilla html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title        = '01-AngularPruebas';
   color:String = 'red';
+
+  generarRandom():String{
+    return Math.floor( Math.random() * 255).toString(16);
+  }
+
+  colorHex():String{
+    return "#" + this.generarRandom() + this.generarRandom() + this.generarRandom();
+  }
+
 }
